f84fbae3175c7e7852919bfbec2e2d4f62e40570
Kevin <v3lkin@gmail.com>
2022-02-13 16:36:55 +0000

    Renaming test files. Fixing user index.ts

--- packages/domains/user/index.ts
+++ packages/domains/user/index.ts
@@ -1,6 +1,6 @@
-import { UserFormatter } from './formatter';
-import { UserController } from './model';
-import { UserRouter } from './routes/v1-get-user';
+import { UserFormatter } from './UserFormatter';
+import { UserController } from './UserController';
+import { UserRouter } from './routes/UserRouter';
 import { IUserRepository, IUserRouter } from './types';
 
 export function createRouter(repository: IUserRepository, version: number = 1): IUserRouter {
--- packages/domains/user/test/UserFormatter.test.ts
+++ packages/domains/user/test/UserFormatter.test.ts
@@ -1,26 +1,28 @@
-import { capitalize, format, secureTrim } from '../formatter';
+import { UserFormatter } from '../UserFormatter';
 
 describe('[Packages | User-domain | Formatter] capitalize', () => {
+  let formatter: UserFormatter = new UserFormatter();
   test('capitalize should make the first character as a capital letter', () => {
-    return expect(capitalize('mario')).toEqual('Mario');
+    return expect(formatter.capitalize('mario')).toEqual('Mario');
   });
 
   test('capitalize should do nothing on already capitalized word', () => {
-    return expect(capitalize('Mario')).toEqual('Mario');
+    return expect(formatter.capitalize('Mario')).toEqual('Mario');
   });
 
   test('capitalize should do nothing on numbers', () => {
-    return expect(capitalize(123)).toEqual('123');
+    return expect(formatter.capitalize(123)).toEqual('123');
   });
 
   test('capitalize should do nothing on strings of numbers', () => {
-    return expect(capitalize('123')).toEqual('123');
+    return expect(formatter.capitalize('123')).toEqual('123');
   });
 });
 
 describe('[Packages | User-domain | Formatter] secureTrim', () => {
+  let formatter: UserFormatter = new UserFormatter();
   test('secureTrim should remove fields that are not defined in the list of public fields', () => {
-    return expect(secureTrim({
+    return expect(formatter.secureTrim({
       id: '1',
       first_name: 'John',
       last_name: 'Smith',
@@ -35,8 +37,9 @@ describe('[Packages | User-domain | Formatter] secureTrim', () => {
 });
 
 describe('[Packages | User-domain | Formatter] format', () => {
+  let formatter: UserFormatter = new UserFormatter();
   test('format should return an instance of users that fits the API model, based on the db raw value', () => {
-    return expect(format({
+    return expect(formatter.format({
       first_name: 'john',
       last_name: 'smith',
       company_name: 'Pleo',
