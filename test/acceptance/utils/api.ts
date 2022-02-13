598878436ab9179ff3f0d7360c9d4ace6b588b5a
Kevin <v3lkin@gmail.com>
2022-02-13 16:10:39 +0000

    Finalizing IoC and renaming files.

--- test/acceptance/utils/api.ts
+++ test/acceptance/utils/api.ts
@@ -1,6 +1,7 @@
 import { ExpenseRepository } from '@nc/domain-expense/data/ExpenseRepository';
+import { UserRepository } from '@nc/domain-user/data/UserRepository';
 import { agent } from 'supertest';
 import { createServer } from '../../../server';
 
 //TODO: Mock repository up.
-export const Api = agent(createServer(new ExpenseRepository()));
+export const Api = agent(createServer(new UserRepository(), new ExpenseRepository()));
